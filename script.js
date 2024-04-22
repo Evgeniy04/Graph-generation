47baddf40685f438e4305176038f58953a63fed4
Evgeniy04 <evgenyiavra@gmail.com>
2024-04-22 21:43:03 +0700

    fix createIncidenceMatrix

--- script.js
+++ script.js
@@ -3,52 +3,46 @@ form.addEventListener("submit", generate);
 let isDirected;
 
 function generate(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  // Получаем значения формы
-  const formData = new FormData(form),
-    dimension = parseInt(formData.get("dimension")),
-    fullGraph = formData.get("btncheckfull"),
-    multipleEdgeGraph = formData.get("btncheckmultiples"),
-    loopsGraph = formData.get("btncheckloops");
+	// Получаем значения формы
+	const formData = new FormData(form),
+		dimension = parseInt(formData.get("dimension")),
+		fullGraph = formData.get("btncheckfull"),
+		multipleEdgeGraph = formData.get("btncheckmultiples"),
+		loopsGraph = formData.get("btncheckloops");
 
-  let adjacencyMatrix = fullGraph
-    ? generateAdjacencyMatrixOfCompleteGraph(dimension)
-    : generateAdjacencyMatrix(dimension);
+	let adjacencyMatrix = fullGraph
+		? generateAdjacencyMatrixOfCompleteGraph(dimension)
+		: generateAdjacencyMatrix(dimension);
 
-  // Параметр, определяющий тип графа (true - ориентированный, false - неориентированный)
-  isDirected = determineGraphType(adjacencyMatrix);
+	// Параметр, определяющий тип графа (true - ориентированный, false - неориентированный)
+	isDirected = determineGraphType(adjacencyMatrix);
 
-  // Добавляем к матрице кратные рёбра и/или петли
-  adjacencyMatrix = multipleEdgeGraph
-    ? addMultipleEdges(adjacencyMatrix)
-    : adjacencyMatrix;
-  adjacencyMatrix = loopsGraph ? addLoops(adjacencyMatrix) : adjacencyMatrix;
+	// Добавляем к матрице кратные рёбра и/или петли
+	adjacencyMatrix = multipleEdgeGraph ? addMultipleEdges(adjacencyMatrix) : adjacencyMatrix;
+	adjacencyMatrix = loopsGraph ? addLoops(adjacencyMatrix) : adjacencyMatrix;
 
-  // Делаем кнопки показа матриц видимыми
-  document
-    .querySelector('button[data-bs-target="#showAdjacencyMatrixModal"]')
-    .classList.remove("d-none");
-  document
-    .querySelector('button[data-bs-target="#showIncidenceMatrixModal"]')
-    .classList.remove("d-none");
+	// Делаем кнопки показа матриц видимыми
+	document
+		.querySelector('button[data-bs-target="#showAdjacencyMatrixModal"]')
+		.classList.remove("d-none");
+	document
+		.querySelector('button[data-bs-target="#showIncidenceMatrixModal"]')
+		.classList.remove("d-none");
 
-  // Отображение матрицы смежности
-  let theadAdjacencyMatrix = adjacencyMatrix
-    .map((_, i) => `<th scope="col">${i}</th>`)
-    .join("");
-  let contentAdjacencyMatrix = "";
-  for (let i = 0; i < dimension; i++) {
-    contentAdjacencyMatrix += `<tr><th scope="row">${i}</th>`;
-    for (let j = 0; j < dimension; j++) {
-      contentAdjacencyMatrix += `<td>${adjacencyMatrix[i][j]}</td>`;
-    }
-    contentAdjacencyMatrix += `</tr>`;
-  }
-  let tableAdjacencyMatrix = document.querySelector(
-    "#showAdjacencyMatrixModal table.table"
-  );
-  tableAdjacencyMatrix.innerHTML = `<thead>
+	// Отображение матрицы смежности
+	let theadAdjacencyMatrix = adjacencyMatrix.map((_, i) => `<th scope="col">${i}</th>`).join("");
+	let contentAdjacencyMatrix = "";
+	for (let i = 0; i < dimension; i++) {
+		contentAdjacencyMatrix += `<tr><th scope="row">${i}</th>`;
+		for (let j = 0; j < dimension; j++) {
+			contentAdjacencyMatrix += `<td>${adjacencyMatrix[i][j]}</td>`;
+		}
+		contentAdjacencyMatrix += `</tr>`;
+	}
+	let tableAdjacencyMatrix = document.querySelector("#showAdjacencyMatrixModal table.table");
+	tableAdjacencyMatrix.innerHTML = `<thead>
         <tr>
             <th scope="col">Вершина</th>
             ${theadAdjacencyMatrix}
@@ -58,63 +52,56 @@ function generate(event) {
         ${contentAdjacencyMatrix}
         </tbody>`;
 
-  // Создаем массив узлов и массив ребер на основе матрицы смежности
-  let nodes = [];
-  let edges = [];
-  let edgeCount = 0;
-  for (let i = 0; i < dimension; i++) {
-    nodes.push({ id: i, label: String(i) });
-    // Если граф ориентированный, то рассматриваем все пересечения с другим элементом построчно
-    // Если граф неориентированный, то смотрим только главную диагональ матрицы и под ней
-    let lim = isDirected ? dimension : i + 1;
-    for (let j = 0; j < lim; j++) {
-      const count = adjacencyMatrix[i][j];
-      if (count <= 0) continue;
-      for (let k = 0; k < count; k++) {
-        edges.push({ id: edgeCount, from: i, to: j, label: `e${edgeCount}` });
-        edgeCount++;
-      }
-    }
-  }
+	// Создаем массив узлов и массив ребер на основе матрицы смежности
+	let nodes = [];
+	let edges = [];
+	let edgeCount = 0;
+	for (let i = 0; i < dimension; i++) {
+		nodes.push({ id: i, label: String(i) });
+		for (let j = i; j < dimension; j++) {
+			const count = adjacencyMatrix[i][j];
+			if (count <= 0) continue;
+			for (let k = 0; k < count; k++) {
+				edges.push({ id: edgeCount, from: i, to: j, label: `e${edgeCount}` });
+				edgeCount++;
+			}
+		}
+	}
 
-  // Создаем объект данных для графа
-  const data = {
-    nodes: new vis.DataSet(nodes),
-    edges: new vis.DataSet(edges),
-  };
+	// Создаем объект данных для графа
+	const data = {
+		nodes: new vis.DataSet(nodes),
+		edges: new vis.DataSet(edges),
+	};
 
-  // Рисуем направление, если граф ориентированный
-  const options = {
-    edges: {
-      arrows: {
-        to: isDirected,
-      },
-    },
-  };
+	// Рисуем направление, если граф ориентированный
+	const options = {
+		edges: {
+			arrows: {
+				to: isDirected,
+			},
+		},
+	};
 
-  // Создаем граф
-  const container = document.getElementById("network");
-  new vis.Network(container, data, options);
+	// Создаем граф
+	const container = document.getElementById("network");
+	new vis.Network(container, data, options);
 
-  // Создаем матрицу инцидентности
-  const incidenceMatrix = createIncidenceMatrix(adjacencyMatrix, edges);
+	// Создаем матрицу инцидентности
+	const incidenceMatrix = createIncidenceMatrix(adjacencyMatrix, edges);
 
-  // Отображение матрицы инцидентности
-  let theadIncidenceMatrix = edges
-    .map((v) => `<th scope="col">${v.label}</th>`)
-    .join("");
-  let contentIncidenceMatrix = "";
-  for (let i = 0; i < dimension; i++) {
-    contentIncidenceMatrix += `<tr><th scope="row">${i}</th>`;
-    for (let j = 0; j < edges.length; j++) {
-      contentIncidenceMatrix += `<td>${incidenceMatrix[i][j]}</td>`;
-    }
-    contentIncidenceMatrix += `</tr>`;
-  }
-  let tableIncidenceMatrix = document.querySelector(
-    "#showIncidenceMatrixModal table.table"
-  );
-  tableIncidenceMatrix.innerHTML = `<thead>
+	// Отображение матрицы инцидентности
+	let theadIncidenceMatrix = edges.map((v) => `<th scope="col">${v.label}</th>`).join("");
+	let contentIncidenceMatrix = "";
+	for (let i = 0; i < dimension; i++) {
+		contentIncidenceMatrix += `<tr><th scope="row">${i}</th>`;
+		for (let j = 0; j < edges.length; j++) {
+			contentIncidenceMatrix += `<td>${incidenceMatrix[i][j]}</td>`;
+		}
+		contentIncidenceMatrix += `</tr>`;
+	}
+	let tableIncidenceMatrix = document.querySelector("#showIncidenceMatrixModal table.table");
+	tableIncidenceMatrix.innerHTML = `<thead>
         <tr>
             <th scope="col">Вершина/Ребро</th>
             ${theadIncidenceMatrix}
@@ -127,72 +114,77 @@ function generate(event) {
 
 // Генерация матрицы смежности ориентированного графа без петель и кратных рёбер
 function generateAdjacencyMatrix(n) {
-  const matrix = new Array(n)
-    .fill(0)
-    .map((_, i) =>
-      [...Array(n)].map((_, j) => (i === j ? 0 : Math.round(Math.random())))
-    );
+	const matrix = Array.from({ length: n }, () => Array.from({ length: n }, () => 0));
+	for (let i = 0; i < n; i++) {
+		for (let j = i + 1; j < n; j++) {
+			matrix[i][j] = Math.round(Math.random());
+			matrix[j][i] = matrix[i][j];
+		}
+	}
 
-  return matrix;
+	return matrix;
 }
 // Генерация матрицы смежности полного графа без петель и кратных рёбер
 function generateAdjacencyMatrixOfCompleteGraph(n) {
-  const matrix = new Array(n)
-    .fill(0)
-    .map((_, i) => [...Array(n)].map((_, j) => (i === j ? 0 : 1)));
+	const matrix = new Array(n).fill(0).map((_, i) => [...Array(n)].map((_, j) => (i === j ? 0 : 1)));
 
-  return matrix;
+	return matrix;
 }
 // Добавить кратные рёбра
 function addMultipleEdges(matrix) {
-  for (let i = 0; i < matrix.length; i++) {
-    let lim = isDirected ? matrix.length : i + 1;
-    for (let j = 0; j < lim; j++) {
-      const count = matrix[i][j];
-      if (count <= 0) continue;
-      let randomNumber = Math.round(Math.random()) + 1;
-      matrix[i][j] = randomNumber;
-      matrix[j][i] = isDirected ? matrix[j][i] : randomNumber;
-    }
-  }
+	for (let i = 0; i < matrix.length; i++) {
+		let lim = isDirected ? matrix.length : i + 1;
+		for (let j = 0; j < lim; j++) {
+			const count = matrix[i][j];
+			if (count <= 0) continue;
+			matrix[i][j] = Math.round(Math.random()) + 1;
+			matrix[j][i] = isDirected ? matrix[j][i] : matrix[i][j];
+		}
+	}
 
-  return matrix;
+	return matrix;
 }
 // Добавить петли
 function addLoops(matrix) {
-  matrix = matrix.map((_, i) =>
-    _.map((v, j) => {
-      if (i === j) return Math.round(Math.random());
-      return v;
-    })
-  );
+	matrix = matrix.map((_, i) =>
+		_.map((v, j) => {
+			if (i === j) return Math.round(Math.random());
+			return v;
+		})
+	);
 
-  return matrix;
+	return matrix;
 }
 // Функция для создания матрицы инцидентности
 function createIncidenceMatrix(matrix, edges) {
-  const incidenceMatrix = [];
-  for (let j = 0; j < matrix.length; j++) {
-    incidenceMatrix.push([]);
-    for (let i = 0; i < edges.length; i++) {
-      // Если у вершины есть пересечение с каким-либо ребром, значит 1
-      if (edges[i].from === j || edges[i].to === j) {
-        incidenceMatrix[j].push(1);
-      } else {
-        incidenceMatrix[j].push(0);
-      }
-    }
-  }
-  return incidenceMatrix;
+	const incidenceMatrix = Array.from({ length: matrix.length }, () =>
+		Array.from({ length: edges.length }, () => 0)
+	);
+
+	let edgeCount = 0;
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = i; j < matrix.length; j++) {
+			if (matrix[i][j] > 0) {
+				for (let _ = 0; _ < matrix[i][j]; _++) {
+					incidenceMatrix[i][edgeCount] += 1;
+					incidenceMatrix[j][edgeCount] += 1;
+
+					edgeCount++;
+				}
+			}
+		}
+	}
+
+	return incidenceMatrix;
 }
 // Функция для определения типа графа (ориентированный или неориентированный)
 function determineGraphType(matrix) {
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (matrix[i][j] !== matrix[j][i]) {
-        return true;
-      }
-    }
-  }
-  return false;
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < i; j++) {
+			if (matrix[i][j] !== matrix[j][i]) {
+				return true;
+			}
+		}
+	}
+	return false;
 }
